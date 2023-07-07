5fe22e45963e5f2313b8dafb774c7abf2ca5ab95
Bryce Lampe <bryce@pulumi.com>
2023-07-07 13:07:49 -0700

    Provide an AnalyzerPlugin for use with golangci-lint

--- cmd/unexportedglobal/main.go
+++ cmd/unexportedglobal/main.go
@@ -9,13 +9,35 @@
 // Alternatively, you can use the 'go vet' command:
 //
 //	$ go vet -vettool=$(which unexportedglobal) ./...
+//
+// Alternatively, you can use it as a golangci-lint plugin:
+//
+//	$ go build -buildmode=plugin go.abhg.dev/unexportedglobal/cmd/unexportedglobal
+//	$ cat .golangci.yml
+//	linter-settings:
+//	  custom:
+//	    unexportedglobal:
+//	      path: unexportedglobal.so
+//	      description: Verify unexported globals have an underscore prefix.
+//	      original-url: go.abhg.dev/unexportedglobal
 package main
 
 import (
 	"go.abhg.dev/unexportedglobal"
+	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
 func main() {
 	singlechecker.Main(unexportedglobal.Analyzer)
+}
+
+// AnalyzerPlugin provides the analyzer as a plugin.
+var AnalyzerPlugin analyzerPlugin
+
+type analyzerPlugin struct{}
+
+// GetAnalyzers returns the unexportedglobal analyzer.
+func (*analyzerPlugin) GetAnalyzers() []*analysis.Analyzer {
+	return []*analysis.Analyzer{unexportedglobal.Analyzer}
 }
